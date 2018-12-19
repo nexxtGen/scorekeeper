952624b0c2446f189506b2e48509a28bf50a50c6
Kamil Szczypior <kszczypior1@gmail.com>
2018-12-19 22:03:17 +0100

    Add App component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
 import './App.css';
+import PlayersList from './components/PlayersList/PlayersList';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
-  }
+ constructor() {
+   super();
+
+   this.state = {
+     players: [
+       {
+         name: 'Marian',
+         score: 5,
+       },
+       {
+         name: 'Józef',
+         score: 0,
+       }
+     ]
+   }
+ }
+
+ onScoreUpdate = (playerIndex, scoreChange) => {
+  this.setState({
+    players: this.state.players.map((player, index) => {
+      if (index === playerIndex) {
+        return { ...player, score: player.score + scoreChange };
+      }
+      return player;
+    })
+  })
+}
+
+ render() {
+   return (
+     <div className="App">
+       <PlayersList players={this.state.players} onScoreUpdate={this.onScoreUpdate}/>
+     </div>
+   );
+ }
 }
 
 export default App;
